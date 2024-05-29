f740f76f0ac83e7cd75a27bf438a81782d73b673
mazzinno <abdennasr3@gmail.com>
2024-05-29 16:55:19 +0000

    tast 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -9,4 +9,3 @@ export default function taskBlock(trueOrFalse) {
 
   return [task, task2];
 }
-
--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -7,4 +7,3 @@ export default function getNeighborhoodsList() {
     return self.sanFranciscoNeighborhoods;
   };
 }
-
