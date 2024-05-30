b5b10fd9140404faf9be4497e19e602dc63ac996
mazzinno <abdennasr3@gmail.com>
2024-05-30 16:29:09 +0000

    es6 basics done

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,8 @@
-
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
+    getNumberOfDepartments(list) {
+      return Object.keys(list).length;
     },
   };
 }
-
