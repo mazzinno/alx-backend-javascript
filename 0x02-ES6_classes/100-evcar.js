e35f812fd246d70967d058e63cf32b2d2a3c1289
mazzinno <abdennasr3@gmail.com>
2024-06-01 02:48:45 +0000

    done

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,11 +3,22 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
+    if (typeof range !== 'string') {
+      throw new TypeError('range must be a string');
+    }
     this._range = range;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   cloneCar() {
-    return new (
-      Object.getPrototypeOf(this.constructor))(this._brand, this._motor, this._color, this._range);
+    return new Car();
   }
 }
