945a67c167f8fb9114fabeca1e20b494120fd52e
mazzinno <abdennasr3@gmail.com>
2024-06-04 20:19:51 +0000

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
 export default function getListStudents() {
-    const person = [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ]
-    return person
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
