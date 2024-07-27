8d46e6233883885e6d9854fe081f5982fa2b431e
mazzinno <abdennasr3@gmail.com>
2024-07-27 01:42:00 +0000

    done tasks

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message
+ * @param {String} msg The message to display
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+read me file for 0x05-Node_JS_basic
